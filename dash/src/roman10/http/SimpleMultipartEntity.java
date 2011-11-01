762bad8b520d9dc6785a8babd9aa7201c2cb9652
roman10 <liuf0005@gmail.com>
2011-11-01 13:01:39 -0400

    new files

---
+++ dash/src/roman10/http/SimpleMultipartEntity.java
@@ -0,0 +1,163 @@
+package roman10.http;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.Random;
+
+import org.apache.http.Header;
+import org.apache.http.HttpEntity;
+import org.apache.http.message.BasicHeader;
+import android.util.Log;
+
+public class SimpleMultipartEntity implements HttpEntity {
+
+    private final static char[] MULTIPART_CHARS = "-_1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        .toCharArray();
+
+    private String boundary = null;
+
+    ByteArrayOutputStream out = new ByteArrayOutputStream();
+    boolean isSetLast = false;
+    boolean isSetFirst = false;
+
+    public SimpleMultipartEntity() {
+        final StringBuffer buf = new StringBuffer();
+        final Random rand = new Random();
+        for (int i = 0; i < 30; i++) {
+            buf.append(MULTIPART_CHARS[rand.nextInt(MULTIPART_CHARS.length)]);
+        }
+        this.boundary = buf.toString();
+
+    }
+
+    public void writeFirstBoundaryIfNeeds(){
+        if(!isSetFirst){
+            try {
+                out.write(("--" + boundary + "\r\n").getBytes());
+            } catch (final IOException e) {
+                Log.e("SimpleMultipartEntity-writeFirstBoundaryIfNeeds", e.getMessage(), e);
+            }
+        }
+        isSetFirst = true;
+    }
+
+    public void writeLastBoundaryIfNeeds() {
+        if(isSetLast){
+            return ;
+        }
+        try {
+            out.write(("\r\n--" + boundary + "--\r\n").getBytes());
+        } catch (final IOException e) {
+            Log.e("SimpleMultipartEntity-writeLastBoundaryIfNeeds", e.getMessage(), e);
+        }
+        isSetLast = true;
+    }
+
+    public void addPart(final String key, final String value) {
+        writeFirstBoundaryIfNeeds();
+        try {
+            out.write(("Content-Disposition: form-data; name=\"" +key+"\"\r\n").getBytes());
+            out.write("Content-Type: text/plain; charset=UTF-8\r\n".getBytes());
+            out.write("Content-Transfer-Encoding: 8bit\r\n\r\n".getBytes());
+            out.write(value.getBytes());
+            out.write(("\r\n--" + boundary + "\r\n").getBytes());
+        } catch (final IOException e) {
+            Log.e("SimpleMultipartEntity-addPart", e.getMessage(), e);
+        }
+    }
+
+    public void addPart(final String key, final String fileName, final InputStream fin){
+        addPart(key, fileName, fin, "application/octet-stream");
+    }
+
+    public void addPart(final String key, final String fileName, final InputStream fin, String type){
+        writeFirstBoundaryIfNeeds();
+        try {
+            type = "Content-Type: "+type+"\r\n";
+            out.write(("Content-Disposition: form-data; name=\""+ key+"\"; filename=\"" + fileName + "\"\r\n").getBytes());
+            out.write(type.getBytes());
+            out.write("Content-Transfer-Encoding: binary\r\n\r\n".getBytes());
+
+            final byte[] tmp = new byte[4096];
+            int l = 0;
+            while ((l = fin.read(tmp)) != -1) {
+                out.write(tmp, 0, l);
+            }
+            out.flush();
+        } catch (final IOException e) {
+            Log.e("SimpleMultipartEntity-addPart", e.getMessage(), e);
+        } finally {
+            try {
+                fin.close();
+            } catch (final IOException e) {
+                Log.e("SimpleMultipartEntity-addPart", e.getMessage(), e);
+            }
+        }
+    }
+
+    public void addPart(final String key, final File value) {
+        try {
+            addPart(key, value.getName(), new FileInputStream(value));
+        } catch (final FileNotFoundException e) {
+            Log.e("SimpleMultipartEntity-addPart", e.getMessage(), e);
+        }
+    }
+
+    //@Override
+    public long getContentLength() {
+        writeLastBoundaryIfNeeds();
+        return out.toByteArray().length;
+    }
+
+    //@Override
+    public Header getContentType() {
+        return new BasicHeader("Content-Type", "multipart/form-data; boundary=" + boundary);
+    }
+
+    //@Override
+    public boolean isChunked() {
+        return false;
+    }
+
+    //@Override
+    public boolean isRepeatable() {
+        return false;
+    }
+
+    //@Override
+    public boolean isStreaming() {
+        return false;
+    }
+
+    //@Override
+    public void writeTo(final OutputStream outstream) throws IOException {
+        outstream.write(out.toByteArray());
+    }
+
+    //@Override
+    public Header getContentEncoding() {
+        return null;
+    }
+
+    //@Override
+    public void consumeContent() throws IOException,
+    UnsupportedOperationException {
+        if (isStreaming()) {
+            throw new UnsupportedOperationException(
+            "Streaming entity does not implement #consumeContent()");
+        }
+    }
+
+    //@Override
+    public InputStream getContent() throws IOException,
+    UnsupportedOperationException {
+        return new ByteArrayInputStream(out.toByteArray());
+    }
+
+}
---
+++ dash/src/roman10/http/UploadService.java
@@ -0,0 +1,131 @@
+package roman10.http;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
+import org.apache.http.HttpVersion;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.FileEntity;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.params.CoreProtocolPNames;
+
+
+import roman10.media.dash.VideoBrowser;
+
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.IBinder;
+import android.os.PowerManager;
+import android.util.Log;
+
+public class UploadService extends Service {
+	private Context mContext;
+	private PowerManager.WakeLock wl;	//lock used when uploading is on-going  
+	
+	@Override
+	public void onCreate() {
+		mContext = this.getApplicationContext();
+	}
+	
+	@Override
+	public void onStart(Intent intent, int startid) {
+		Log.i("onStart", "start upload files");
+		UploadFileTask uploadFileTask = new UploadFileTask();
+		uploadFileTask.execute(null);
+	}
+	@Override
+	public IBinder onBind(Intent intent) {
+		return null;
+	}
+	
+	private void stopService() {
+		this.stopSelf();
+	}
+	
+	@Override
+	public void onDestroy() {
+		super.onDestroy();
+		if (wl != null) {
+			wl.release();
+		}
+	}
+	
+	//TODO: to make it more efficient, don't create http client every time
+	public void uploadFile(String pFileFullPathName) throws ClientProtocolException, IOException {
+		HttpClient httpClient = new DefaultHttpClient();
+		try {
+			httpClient.getParams().setParameter(CoreProtocolPNames.PROTOCOL_VERSION, HttpVersion.HTTP_1_1);
+			httpClient.getParams().setParameter("http.socket.timeout", new Integer(90000)); // 90 second 
+			
+			HttpPost httpPost = new HttpPost("http://cervino.ddns.comp.nus.edu.sg/~a0075306/receive.php");
+//			File lFile = new File(pFileFullPathName);
+//			FileEntity lEntity;
+//			lEntity = new FileEntity(lFile, "binary/octet-stream"); 
+//			lEntity.setChunked(true);
+			
+			SimpleMultipartEntity mpEntity = new SimpleMultipartEntity();
+			File vFile = new File(pFileFullPathName);
+		    mpEntity.addPart("myfile", pFileFullPathName.substring(pFileFullPathName.lastIndexOf("/")+1), new FileInputStream(vFile), "video/" + pFileFullPathName.substring(pFileFullPathName.lastIndexOf(".") + 1));
+			
+			httpPost.setEntity(mpEntity);
+			HttpResponse lResponse = httpClient.execute(httpPost);
+			if (lResponse.getStatusLine().getStatusCode() != HttpStatus.SC_OK) { 
+                Log.e("UploadService-uploadFile","Response Status line code:"+ lResponse.getStatusLine()); 
+		    }
+			HttpEntity resEntity = lResponse.getEntity(); 
+            if (resEntity == null) { 
+                Log.e("UploadService-uploadFile", "No Response!"); 
+            } 
+		} finally {
+			httpClient.getConnectionManager().shutdown(); 
+		}
+	}
+	
+	private class UploadFileTask extends AsyncTask<Object, Integer, Object> {
+		@Override
+		protected void onPreExecute() {
+			/*acquire PARTIAL_WAKE_LOCK WakeLock, so that the processing can continue even when screen is off*/
+			PowerManager pm = (PowerManager) getSystemService(Context.POWER_SERVICE);
+			wl = pm.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK, 
+					"generateStreamletTask");
+			wl.acquire();
+		}
+		@Override
+		protected Object doInBackground(Object... arg0) {
+			 try {
+				VideoBrowser.mCurrUploadFileNum = 0;
+				for (int fi = 0; fi < VideoBrowser.mUploadFileNameList.size(); ++fi) {
+					String lFileFullPathName = VideoBrowser.mUploadFileNameList.get(fi);
+					String lFileNameWithoutExt = lFileFullPathName.substring(lFileFullPathName.lastIndexOf("/") + 1, lFileFullPathName.lastIndexOf("."));
+					Log.i("UploadFileTask-doInBackground", "upload file " + lFileFullPathName);
+					uploadFile(lFileFullPathName);
+					++VideoBrowser.mCurrUploadFileNum;
+					this.publishProgress();
+				}
+	        } catch (Exception e) {
+				Log.e("UploadFileTask-doInBackground", e.getMessage());
+			}
+			return null;
+		}
+		
+		@Override
+		protected void onProgressUpdate(Integer... progress) {
+			VideoBrowser.updateUploadFilesProgress();
+		}
+		
+		@Override
+		protected void onPostExecute(Object res) {
+			VideoBrowser.finishUploadFiles();
+			stopService();		//finish the service
+		}
+	}
+
+}
